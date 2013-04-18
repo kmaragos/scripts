cc2be30e86086758bffaa15cad1d49fc54713063
Konstantinos Maragos <kmaragos@gmail.com>
2013-04-19 02:25:08 +0300

    add count in configuration

--- bak_foursquare.sh
+++ bak_foursquare.sh
@@ -4,6 +4,6 @@
 source conf/conf_general.sh
 
 # download foursquare user feeds (KML, RSS & ICS)
-wget https://feeds.foursquare.com/history/$FOURSQUARE_TOKEN.kml?count=50000 -q -O $FOURSQUARE_BAK_DIR/$FOURSQUARE_USER.kml
-wget https://feeds.foursquare.com/history/$FOURSQUARE_TOKEN.rss?count=50000 -q -O $FOURSQUARE_BAK_DIR/$FOURSQUARE_USER.rss
+wget https://feeds.foursquare.com/history/$FOURSQUARE_TOKEN.kml?count=$FOURSQUARE_COUNT -q -O $FOURSQUARE_BAK_DIR/$FOURSQUARE_USER.kml
+wget https://feeds.foursquare.com/history/$FOURSQUARE_TOKEN.rss?count=$FOURSQUARE_COUNT -q -O $FOURSQUARE_BAK_DIR/$FOURSQUARE_USER.rss
 wget https://feeds.foursquare.com/history/$FOURSQUARE_TOKEN.ics -q -O $FOURSQUARE_BAK_DIR/$FOURSQUARE_USER.ics
